46aee34f720a2bceef181bd3eca612565fe52728
Anwesh43 <anweshthecool0@gmail.com>
2018-08-04 11:35:58 +0530

    importing libraries and defining nodes

--- roundrectfillerview/src/main/java/com/anwesh/uiprojects/roundrectfillerview/RoundRectFillerView.kt
+++ roundrectfillerview/src/main/java/com/anwesh/uiprojects/roundrectfillerview/RoundRectFillerView.kt
@@ -3,3 +3,11 @@ package com.anwesh.uiprojects.roundrectfillerview
 /**
  * Created by anweshmishra on 04/08/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.content.Context
+
+val nodes : Int = 5
