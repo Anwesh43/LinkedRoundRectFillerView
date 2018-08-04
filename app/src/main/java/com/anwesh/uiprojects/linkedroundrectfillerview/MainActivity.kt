62c0773c556b03e894715513647ffaa616d894f9
Anwesh43 <anweshthecool0@gmail.com>
2018-08-04 15:28:51 +0530

    using AnimationListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedroundrectfillerview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedroundrectfillerview/MainActivity.kt
@@ -3,14 +3,28 @@ package com.anwesh.uiprojects.linkedroundrectfillerview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.roundrectfillerview.RoundRectFillerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        RoundRectFillerView.create(this)
+        val view: RoundRectFillerView = RoundRectFillerView.create(this)
         fullScreen()
+        view.addAnimationListener({ completeToast(it) }, { resetToast(it) })
+    }
+
+    fun createToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    fun completeToast(i: Int) {
+        createToast("animation number ${i + 1} is complete")
+    }
+
+    fun resetToast(i: Int) {
+        createToast("animation number ${i + 1} is reset")
     }
 }
 
