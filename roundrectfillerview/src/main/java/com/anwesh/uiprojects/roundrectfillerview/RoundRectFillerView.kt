e359ba5b2baaf7ed48f216c285a9ac56699bb375
Anwesh43 <anweshthecool0@gmail.com>
2018-08-04 14:10:42 +0530

    adding LinkedRRF class

--- roundrectfillerview/src/main/java/com/anwesh/uiprojects/roundrectfillerview/RoundRectFillerView.kt
+++ roundrectfillerview/src/main/java/com/anwesh/uiprojects/roundrectfillerview/RoundRectFillerView.kt
@@ -145,4 +145,28 @@ class RoundRectFillerView (ctx : Context) : View(ctx) {
             next?.draw(canvas, paint)
         }
     }
+
+    data class LinkedRRF(var i : Int) {
+
+        private var curr : RRFNode = RRFNode(0)
+
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
