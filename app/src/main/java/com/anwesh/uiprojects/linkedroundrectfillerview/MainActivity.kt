e8c1fb6723db9ebf95399330b54daa4eac7f7c84
Anwesh43 <anweshthecool0@gmail.com>
2018-08-04 14:50:26 +0530

    adding View in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedroundrectfillerview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedroundrectfillerview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedroundrectfillerview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.roundrectfillerview.RoundRectFillerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RoundRectFillerView.create(this)
     }
 }
